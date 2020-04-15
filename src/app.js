d38d9bd880e96b7cb52f25e9934a17ef9efe56b3
unknown <marcotelles123@gmail.com>
2020-04-14 22:28:51 -0300

    adicionado tracker e validador de id

--- src/app.js
+++ src/app.js
@@ -1,13 +1,37 @@
 const express = require("express");
 const cors = require("cors");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(logRequests);
+app.put("/repositories/:id", validateRepositoryId);
 
 const repositories = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+  console.log(logLabel);
+
+  next();
+  console.timeEnd(logLabel);
+}
+
+function validateRepositoryId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Id em formato inválido" });
+  }
+
+  return next();
+}
 
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
@@ -36,7 +60,7 @@ app.put("/repositories/:id", (request, response) => {
   );
 
   if (repositoryIndex < 0) {
-    response.status(400).json("Repository Not Found");
+    return response.status(400).json("Repository Not Found");
   }
   console.log(repositories[repositoryIndex]);
   const repository = {
@@ -51,7 +75,7 @@ app.put("/repositories/:id", (request, response) => {
   return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
+app.delete("/repositories/:id", validateRepositoryId, (request, response) => {
   const { id } = request.params;
   const repositoryIndex = repositories.findIndex(
     (repository) => repository.id === id
@@ -66,10 +90,10 @@ app.delete("/repositories/:id", (request, response) => {
   return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
+app.post("/repositories/:id/like", validateRepositoryId, (request, response) => {
   const { id } = request.params;
   const repository = repositories.find((repository) => repository.id === id);
- 
+
   if (repository === undefined) {
     response.status(400).json("Project Not Found");
   }
